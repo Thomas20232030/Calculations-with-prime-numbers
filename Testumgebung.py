4bfa35c76136b2553aec4cf2abd79bd6ad014ff3
Thomas Hoppe <hoppe@telebel.de>
2021-11-08 07:28:27 +0100

    Die Funktion eratosthenes geändert

--- Testumgebung.py
+++ Testumgebung.py
@@ -16,36 +16,19 @@ def eratosthenes(end):
     return [x for x in range(end) if primzahlliste[x] == True]
 
 
-def atkins(limit):
-    results = [2, 3, 5]
-    sieve = [False] * (limit + 1)
-    factor = int(math.sqrt(limit)) + 1
-    for i in range(1, factor):
-        for j in range(1, factor):
-            n = 4 * i ** 2 + j ** 2
-            if (n <= limit) and (n % 12 == 1 or n % 12 == 5):
-                sieve[n] = not sieve[n]
-            n = 3 * i ** 2 + j ** 2
-            if (n <= limit) and (n % 12 == 7):
-                sieve[n] = not sieve[n]
-            if i > j:
-                n = 3 * i ** 2 - j ** 2
-                if (n <= limit) and (n % 12 == 11):
-                    sieve[n] = not sieve[n]
-    for index in range(5, factor):
-        if sieve[index]:
-            for jndex in range(index ** 2, limit, index ** 2):
-                sieve[jndex] = False
-    for index in range(7, limit):
-        if sieve[index]:
-            results.append(index)
-    return results
+def primes(limit):
+    is_prime = [False] * 2 + [True] * (limit - 1)
+    for n in range(int(limit**0.5 + 1.5)): # stop at ``sqrt(limit)``
+        if is_prime[n]:
+            for i in range(n*n, limit+1, n):
+                is_prime[i] = False
+    return [i for i, prime in enumerate(is_prime) if prime]
 
 
-for grenze in range(100000, 1000000, 100000):
+for grenze in range(1000000, 10000000, 1000000):
 
     asz = time.time()
-    atkins(grenze)
+    primes(grenze)
     aez = time.time()
     ea = (aez - asz)
 
@@ -54,4 +37,5 @@ for grenze in range(100000, 1000000, 100000):
     eez = time.time()
     ee = eez - esz
 
-    print(grenze, " / Atkins:", ea, "/ Eratosthenes:", ee, "Prozent = ", (ea / ee) * 100, "%")
+    print(grenze, " / E1:", ea, "/ E2:", ee, "Prozent = ", (ea / ee) * 100, "%")
+
